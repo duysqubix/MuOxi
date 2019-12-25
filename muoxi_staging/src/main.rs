31f47c5512128799ff1bbd10b9c351a265975530
duysqubix <duys@qubixat.com>
2019-12-25 16:12:52 -0700

    Created bare bones for Staging Server - at this point it purely acts as a proxy server that will point to the server upon where the game actually takes place

---
+++ muoxi_staging/src/main.rs
@@ -0,0 +1,54 @@
+//!
+//! Main Proxy Staging TCP Client communicates with the Game Engine and relays
+//! game engine response to connected clients.
+//! 
+
+use tokio::io;
+use tokio::net::{TcpListener, TcpStream};
+
+use futures::future::try_join;
+use futures::FutureExt;
+
+use std::env;
+use std::error::Error;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box< dyn Error>>{
+    env_logger::init();
+
+    let listen_addr = "127.0.0.1:8000".to_string();
+    let game_addr = "127.0.0.1:4567".to_string();
+
+    println!("TCP Client listening on {} proxying to {}", listen_addr, game_addr);
+
+    let mut listener = TcpListener::bind(&listen_addr).await?;
+
+    while let Ok((inbound, _)) = listener.accept().await{
+        // do proxy work here.
+        let proxy = transfer(inbound, game_addr.clone()).map(|r|{
+            if let Err(e) = r {
+                println!("Failed to transfer; error={}", e);
+            }
+        });
+        
+        tokio::spawn(proxy);
+    }
+
+    Ok(())
+}
+
+async fn transfer(mut inbound: TcpStream, game_addr: String) -> Result<(), Box<dyn Error>>{
+    let mut outbound = TcpStream::connect(&game_addr).await?;
+
+    let (mut ri, mut wi) = inbound.split();
+    let (mut ro, mut wo) = outbound.split();
+
+    let client_to_server = io::copy(&mut ri, &mut wo);
+    let server_to_client = io::copy(&mut ro, &mut wi);
+
+
+    try_join(client_to_server, server_to_client).await?;
+
+    Ok(())
+}
