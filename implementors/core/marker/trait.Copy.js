2e42e00db334f6ff8b5c692b86ead62b44deb5e3
duysqubix <duys@qubixat.com>
2020-01-08 08:01:31 -0700

    added a delete

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["db"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"db/schema/clients/struct.table.html\" title=\"struct db::schema::clients::table\">table</a>",synthetic:false,types:["db::schema::clients::table"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"db/schema/clients/columns/struct.star.html\" title=\"struct db::schema::clients::columns::star\">star</a>",synthetic:false,types:["db::schema::clients::columns::star"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"db/schema/clients/columns/struct.uid.html\" title=\"struct db::schema::clients::columns::uid\">uid</a>",synthetic:false,types:["db::schema::clients::columns::uid"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"db/schema/clients/columns/struct.ip.html\" title=\"struct db::schema::clients::columns::ip\">ip</a>",synthetic:false,types:["db::schema::clients::columns::ip"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"db/schema/clients/columns/struct.port.html\" title=\"struct db::schema::clients::columns::port\">port</a>",synthetic:false,types:["db::schema::clients::columns::port"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/ops/arith/trait.Div.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["db"] = [{text:"impl&lt;Rhs&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a>&lt;Rhs&gt; for <a class=\"struct\" href=\"db/schema/clients/columns/struct.uid.html\" title=\"struct db::schema::clients::columns::uid\">uid</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Rhs: AsExpression&lt;&lt;&lt;<a class=\"struct\" href=\"db/schema/clients/columns/struct.uid.html\" title=\"struct db::schema::clients::columns::uid\">uid</a> as Expression&gt;::SqlType as Div&gt;::Rhs&gt;,&nbsp;</span>",synthetic:false,types:["db::schema::clients::columns::uid"]},{text:"impl&lt;Rhs&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a>&lt;Rhs&gt; for <a class=\"struct\" href=\"db/schema/clients/columns/struct.port.html\" title=\"struct db::schema::clients::columns::port\">port</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Rhs: AsExpression&lt;&lt;&lt;<a class=\"struct\" href=\"db/schema/clients/columns/struct.port.html\" title=\"struct db::schema::clients::columns::port\">port</a> as Expression&gt;::SqlType as Div&gt;::Rhs&gt;,&nbsp;</span>",synthetic:false,types:["db::schema::clients::columns::port"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
